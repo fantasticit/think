4a4d0d2133badc26af7bfa547daaeca8cc226f18
fantasticit <fantasticit@163.com>
2022-03-29 14:54:51 +0800

    fix: disable enter keyevent in title

--- packages/client/src/tiptap/services/node.ts
+++ packages/client/src/tiptap/services/node.ts
@@ -1,4 +1,5 @@
 import { Node } from 'prosemirror-model';
+import { EditorState } from 'prosemirror-state';
 
 export function isTitleNode(node: Node): boolean {
   return node.type.name === 'title';
@@ -18,4 +19,13 @@ export function isTodoListNode(node: Node): boolean {
 
 export function isListNode(node: Node): boolean {
   return isBulletListNode(node) || isOrderedListNode(node) || isTodoListNode(node);
+}
+
+export function isInTitle(state: EditorState): boolean {
+  const $head = state.selection.$head;
+  for (let d = $head.depth; d > 0; d--) {
+    if ($head.node(d).type === state.schema.nodes.title) {
+      return true;
+    }
+  }
 }
