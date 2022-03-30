a4167fe603bf24f37e06c49e9c797feaf768558f
fantasticit <fantasticit@163.com>
2022-03-30 13:57:50 +0800

    feat: improve date version, add empty content

--- packages/client/src/components/data-render/index.tsx
+++ packages/client/src/components/data-render/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Spin, Typography } from '@douyinfe/semi-ui';
+import { Empty } from 'illustrations/empty';
 
 type RenderProps = React.ReactNode | (() => React.ReactNode);
 
 interface IProps {
   loading: boolean;
   error: Error | null;
+  empty?: boolean;
   loadingContent?: RenderProps;
   errorContent?: RenderProps;
+  emptyContent?: RenderProps;
   normalContent: RenderProps;
 }
 
@@ -21,13 +24,37 @@ const defaultRenderError = (error) => {
   return <Text>{(error && error.message) || '未知错误'}</Text>;
 };
 
+const defaultEmpty = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        position: 'relative',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <div>
+        <Empty />
+      </div>
+      <Text type="tertiary">暂无数据</Text>
+    </div>
+  );
+};
+
 const runRender = (fn, ...args) => (typeof fn === 'function' ? fn.apply(null, args) : fn);
 
 export const DataRender: React.FC<IProps> = ({
   loading,
   error,
+  empty,
   loadingContent = defaultLoading,
   errorContent = defaultRenderError,
+  emptyContent = defaultEmpty,
   normalContent,
 }) => {
   if (loading) {
@@ -36,6 +63,10 @@ export const DataRender: React.FC<IProps> = ({
 
   if (error) {
     return runRender(errorContent, error);
+  }
+
+  if (empty) {
+    return runRender(emptyContent);
   }
 
   return runRender(normalContent);
