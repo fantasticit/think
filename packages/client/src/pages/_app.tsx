537b114f17f04cd27201465d972dbd6300d6c620
fantasticit <fantasticit@163.com>
2022-04-04 21:54:35 +0800

    fix: add meta in _app

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { useSafari100vh } from 'hooks/use-safari-100vh';
 import 'viewerjs/dist/viewer.css';
 import 'styles/globals.scss';
@@ -7,7 +8,14 @@ import 'tiptap/styles/index.scss';
 function MyApp({ Component, pageProps }: AppProps) {
   useSafari100vh();
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
