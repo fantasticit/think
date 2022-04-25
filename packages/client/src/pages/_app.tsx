180eec9c19c56c47bb3be4071ad6fffcf518df4d
fantasticit <fantasticit@163.com>
2022-04-25 21:54:59 +0800

    fix: use theme global

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useSafari100vh } from 'hooks/use-safari-100vh';
+import { useTheme } from 'hooks/use-theme';
 import 'viewerjs/dist/viewer.css';
 import 'styles/globals.scss';
 import 'tiptap/styles/index.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useSafari100vh();
+  useTheme();
 
   return (
     <>
