2095cafec24c4e16a58503f3a16dbe541e4a0b14
fantasticit <fantasticit@163.com>
2022-04-27 13:01:09 +0800

    refactor: use re-resizeable

--- packages/client/src/components/resizeable/resizeable.tsx
+++ packages/client/src/components/resizeable/resizeable.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useCallback, useState } from 'react';
 import cls from 'classnames';
-import { useClickOutside } from 'hooks/use-click-outside';
-import interact from 'interactjs';
 import styles from './style.module.scss';
+
+import { Resizable } from 're-resizable';
 
 interface IProps {
   width: number;
@@ -15,71 +15,37 @@ interface IProps {
 const MIN_WIDTH = 50;
 const MIN_HEIGHT = 50;
 
-export const Resizeable: React.FC<IProps> = ({ width, height, className, onChange, onChangeEnd, children }) => {
-  const $container = useRef<HTMLDivElement>(null);
-  const $topLeft = useRef<HTMLDivElement>(null);
-  const $topRight = useRef<HTMLDivElement>(null);
-  const $bottomLeft = useRef<HTMLDivElement>(null);
-  const $bottomRight = useRef<HTMLDivElement>(null);
+export const Resizeable: React.FC<IProps> = ({
+  width: defaultWidth,
+  height: defaultHeight,
+  className,
+  onChange,
+  onChangeEnd,
+  children,
+}) => {
+  const [width, setWidth] = useState(defaultWidth);
+  const [height, setHeight] = useState(defaultHeight);
 
-  useClickOutside($container, {
-    in: () => $container.current.classList.add(styles.isActive),
-    out: () => $container.current.classList.remove(styles.isActive),
-  });
-
-  useEffect(() => {
-    interact($container.current).resizable({
-      edges: {
-        top: true,
-        right: true,
-        bottom: true,
-        left: true,
-      },
-      listeners: {
-        move: function (event) {
-          let { x, y } = event.target.dataset;
-          x = (parseFloat(x) || 0) + event.deltaRect.left;
-          y = (parseFloat(y) || 0) + event.deltaRect.top;
-
-          let { width, height } = event.rect;
-          width = width < MIN_WIDTH ? MIN_WIDTH : width;
-          height = height < MIN_HEIGHT ? MIN_HEIGHT : height;
-
-          Object.assign(event.target.style, {
-            width: `${width}px`,
-            height: `${height}px`,
-            // transform: `translate(${x}px, ${y}px)`,
-          });
-          Object.assign(event.target.dataset, { x, y });
-          onChange && onChange({ width, height });
-        },
-        end: function (event) {
-          let { width, height } = event.rect;
-          onChangeEnd && onChangeEnd({ width, height });
-        },
-      },
-    });
-  }, []);
-
-  useEffect(() => {
-    Object.assign($container.current.style, {
-      width: `${width}px`,
-      height: `${height}px`,
-    });
-  }, [width, height]);
+  const onResizeStop = useCallback(
+    (e, direction, ref, d) => {
+      const nextWidth = width + d.width;
+      const nextHeight = height + d.height;
+      setWidth(nextWidth);
+      setHeight(nextHeight);
+      onChangeEnd({ width: nextWidth, height: nextHeight });
+    },
+    [width, height]
+  );
 
   return (
-    <div
-      id="js-resizeable-container"
+    <Resizable
+      size={{ width, height }}
       className={cls(className, styles.resizable)}
-      ref={$container}
-      style={{ width, height }}
+      minWidth={MIN_WIDTH}
+      minHeight={MIN_HEIGHT}
+      onResizeStop={onResizeStop}
     >
-      <span className={styles.resizer + ' ' + styles.topLeft} ref={$topLeft} data-type={'topLeft'}></span>
-      <span className={styles.resizer + ' ' + styles.topRight} ref={$topRight} data-type={'topRight'}></span>
-      <span className={styles.resizer + ' ' + styles.bottomLeft} ref={$bottomLeft} data-type={'bottomLeft'}></span>
-      <span className={styles.resizer + ' ' + styles.bottomRight} ref={$bottomRight} data-type={'bottomRight'}></span>
       {children}
-    </div>
+    </Resizable>
   );
 };
