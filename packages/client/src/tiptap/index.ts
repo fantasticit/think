a4a2205d11eaad663b23b28257b206401c9337b6
fantasticit <fantasticit@163.com>
2022-05-02 17:59:47 +0800

    tiptap: fix https://github.com/ueberdosis/tiptap/issues/1451

--- packages/client/src/tiptap/index.ts
+++ packages/client/src/tiptap/index.ts
@@ -1,3 +1,10 @@
+import { EditorView } from 'prosemirror-view';
+// @see https://github.com/ueberdosis/tiptap/issues/1451
+EditorView.prototype.updateState = function updateState(state) {
+  if (!this.docView) return; // This prevents the matchesNode error on hot reloads
+  this.updateStateInner(state, this.state.plugins != state.plugins);
+};
+
 export { getSchema } from '@tiptap/core';
 export * from './react';
 export * from './start-kit';
