578753c1f6c9d7bfd05015080c4cc2f9494e48cd
fantasticit <fantasticit@163.com>
2022-05-02 15:31:03 +0800

    refactor: remove unused code

--- packages/client/src/tiptap/menubar.tsx
+++ packages/client/src/tiptap/menubar.tsx
@@ -43,11 +43,7 @@ import { Iframe } from './menus/iframe';
 import { Table } from './menus/table';
 import { Mind } from './menus/mind';
 
-import useTilg from 'tilg';
-
 const _MenuBar: React.FC<{ editor: any }> = ({ editor }) => {
-  useTilg();
-
   if (!editor) return null;
 
   return (
