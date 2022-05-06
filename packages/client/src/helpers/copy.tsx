009d7782a4758dab2f420f72be834f248800d108
fantasticit <fantasticit@163.com>
2022-05-06 23:57:55 +0800

    fix: fix copy text

--- packages/client/src/helpers/copy.tsx
+++ packages/client/src/helpers/copy.tsx
@@ -1,16 +1,6 @@
 import _copy from './copy-to-clipboard';
 import { Toast } from '@douyinfe/semi-ui';
 
-interface Options {
-  debug?: boolean;
-  message?: string;
-  format?: string; // MIME type
-  onCopy?: (clipboardData: object) => void;
-}
-
-export function copy(text: string | { text: string; format: string }[], options?: Options) {
-  options = options || {};
-  options.onCopy = options.onCopy || (() => Toast.success(options.message || '复制成功'));
-  options.format = options.format || 'text/plain';
-  return _copy(text, options);
+export function copy(text: string | { text: string; format: string }[]) {
+  return _copy(text, () => Toast.success('复制成功'));
 }
