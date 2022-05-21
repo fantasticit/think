88c2769fa61440da221a185c886e18f07875dae1
fantasticit <fantasticit@163.com>
2022-05-22 00:02:20 +0800

    client: update meta

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Theme.Provider>
         <Component {...pageProps} />
