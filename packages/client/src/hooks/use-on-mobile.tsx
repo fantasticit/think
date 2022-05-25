2b64a7c09f9e27271b0803ed2c18a9f0bd100ce3
fantasticit <fantasticit@163.com>
2022-05-25 13:39:01 +0800

    client: detect window resize

--- packages/client/src/hooks/use-on-mobile.tsx
+++ packages/client/src/hooks/use-on-mobile.tsx
@@ -1,8 +1,25 @@
+import { useEffect } from 'react';
+
 import { createGlobalHook } from './create-global-hook';
 import { useToggle } from './use-toggle';
 
+const PC_MOBILE_CRITICAL_WIDTH = 765;
+
 const useOnMobile = (defaultIsMobile) => {
   const [isMobile, toggleIsMobile] = useToggle(defaultIsMobile);
+
+  useEffect(() => {
+    function handleResize() {
+      toggleIsMobile(window.innerWidth <= PC_MOBILE_CRITICAL_WIDTH);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [toggleIsMobile]);
+
   return {
     isMobile,
     toggleIsMobile,
