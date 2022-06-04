76ccf3892570e8487e872a1b809c91339a10afea
fantasticit <fantasticit@163.com>
2022-06-04 22:25:18 +0800

    server: support tencent cos

--- packages/server/src/helpers/file.helper/index.ts
+++ packages/server/src/helpers/file.helper/index.ts
@@ -3,16 +3,17 @@ import { ConfigService } from '@nestjs/config';
 import { AliyunOssClient } from './aliyun.client';
 import { LocalOssClient } from './local.client';
 import { OssClient } from './oss.client';
+import { TencentOssClient } from './tencent.client';
 
 export { OssClient };
 
 export const getOssClient = (configService: ConfigService): OssClient => {
-  if (configService.get('oss.aliyun.enable')) {
-    return new AliyunOssClient(configService);
+  if (configService.get('oss.tencent.enable')) {
+    return new TencentOssClient(configService);
   }
 
-  if (configService.get('oss.local.enable')) {
-    return new LocalOssClient(configService);
+  if (configService.get('oss.aliyun.enable')) {
+    return new AliyunOssClient(configService);
   }
 
   return new LocalOssClient(configService);
