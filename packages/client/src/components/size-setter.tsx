fbc66731186973b9df56e12b4437fb105ac66665
fantasticit <fantasticit@163.com>
2022-06-05 19:32:36 +0800

    client: update size setter type

--- packages/client/src/components/size-setter.tsx
+++ packages/client/src/components/size-setter.tsx
@@ -2,12 +2,12 @@ import { Button, Dropdown, Form } from '@douyinfe/semi-ui';
 import { FormApi } from '@douyinfe/semi-ui/lib/es/form';
 import { useCallback, useRef } from 'react';
 
-type ISize = { width: number; height: number };
+type ISize = { width: number | string; height: number | string };
 
 interface IProps {
-  width: number;
-  maxWidth?: number;
-  height: number;
+  width: number | string;
+  maxWidth?: number | string;
+  height: number | string;
   onOk: (arg: ISize) => void;
 }
 
@@ -29,8 +29,8 @@ export const SizeSetter: React.FC<IProps> = ({ width, maxWidth, height, onOk, ch
       render={
         <div style={{ padding: '0 12px 12px' }}>
           <Form initValues={{ width, height }} getFormApi={(formApi) => ($form.current = formApi)} labelPosition="left">
-            <Form.InputNumber autofocus label="宽" field="width" {...(maxWidth ? { max: maxWidth } : {})} />
-            <Form.InputNumber label="高" field="height" />
+            <Form.Input autofocus label="宽" field="width" {...(maxWidth ? { max: maxWidth } : {})} />
+            <Form.Input label="高" field="height" />
           </Form>
           <Button size="small" type="primary" theme="solid" htmlType="submit" onClick={handleOk}>
             设置
