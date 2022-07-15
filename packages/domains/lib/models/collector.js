d2e4e88548fd5b5b81223b0e61156e729640e3e4
fantasticit <fantasticit@163.com>
2022-07-15 14:32:21 +0800

    chore: fix run tsc build error in windows

--- packages/domains/lib/models/collector.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.CollectType = void 0;
-var CollectType;
-(function (CollectType) {
-    CollectType["document"] = "document";
-    CollectType["wiki"] = "wiki";
-})(CollectType = exports.CollectType || (exports.CollectType = {}));
