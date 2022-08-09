87a863874d61b6e0dbb83a20fdfd5c30709a0706
fantasticit <fantasticit@163.com>
2022-08-09 21:59:14 +0800

    tiptap: remove dragable for paragraph

--- packages/client/src/tiptap/core/extensions/paragraph.ts
+++ packages/client/src/tiptap/core/extensions/paragraph.ts
@@ -2,6 +2,5 @@ import { mergeAttributes } from '@tiptap/core';
 import TitapParagraph from '@tiptap/extension-paragraph';
 
 export const Paragraph = TitapParagraph.extend({
-  draggable: true,
   selectable: true,
 });
