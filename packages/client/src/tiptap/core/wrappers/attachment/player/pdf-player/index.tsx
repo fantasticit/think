068aa171106b811221588fb643098479cf8ebdd2
fantasticit <fantasticit@163.com>
2022-08-23 10:12:51 +0800

    client: improve pdf viewer

--- packages/client/src/tiptap/core/wrappers/attachment/player/pdf-player/index.tsx
+++ packages/client/src/tiptap/core/wrappers/attachment/player/pdf-player/index.tsx
@@ -1,31 +1,20 @@
-import { Pagination } from '@douyinfe/semi-ui';
-import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
+import { Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import zhCN from '@react-pdf-viewer/locales/lib/zh_CN.json';
+import React from 'react';
 
 import styles from './index.module.scss';
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 interface IProps {
   url: string;
 }
 
 export const PDFPlayer: React.FC<IProps> = ({ url }) => {
-  const [total, setTotal] = useState(1);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setTotal(numPages);
-  }
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   return (
     <div className={styles.playerWrap}>
-      <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <div className={styles.paginationWrap}>
-        <Pagination total={total} pageSize={1} onChange={(page) => setPageNumber(page)} size="small"></Pagination>
-      </div>
+      <Viewer fileUrl={url} localization={zhCN} plugins={[defaultLayoutPluginInstance]} />
     </div>
   );
 };
