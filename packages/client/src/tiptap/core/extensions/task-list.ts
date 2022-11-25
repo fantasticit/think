3e954e655ddcc54892fe9ee6a363896812324412
fantasticit <fantasticit@163.com>
2022-11-25 15:21:10 +0800

    close #208

--- packages/client/src/tiptap/core/extensions/task-list.ts
+++ packages/client/src/tiptap/core/extensions/task-list.ts
@@ -1,4 +1,7 @@
+import { getNodeType } from '@tiptap/core';
 import { TaskList as BuiltInTaskList } from '@tiptap/extension-task-list';
+import { liftListItem } from 'prosemirror-schema-list';
+import { findParentNodeClosestToPos } from 'prosemirror-utils';
 import { PARSE_HTML_PRIORITY_HIGHEST } from 'tiptap/core/constants';
 
 export const TaskList = BuiltInTaskList.extend({
@@ -9,5 +12,24 @@ export const TaskList = BuiltInTaskList.extend({
         priority: PARSE_HTML_PRIORITY_HIGHEST,
       },
     ];
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      ...this.parent?.(),
+      Backspace: ({ editor }) => {
+        const { selection } = editor.state;
+        const { $from } = selection;
+        const maybeTask = findParentNodeClosestToPos($from, (node) => node.type.name === this.name);
+
+        if (maybeTask && maybeTask.node.childCount === 1 && !maybeTask.node.textContent) {
+          const name = this.editor.can().liftListItem('taskItem') ? 'taskItem' : 'listItem';
+          const type = getNodeType(name, editor.view.state.schema);
+          return liftListItem(type)(editor.view.state, editor.view.dispatch);
+        }
+
+        return false;
+      },
+    };
   },
 });
