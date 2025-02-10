b4d8d7f9793384e006ff731a9cf2e4412559a2ef
fantasticit <fantasticit@163.com>
2025-02-10 22:42:12 +0800

    Update file.ts

--- packages/domains/src/api/file.ts
+++ packages/domains/src/api/file.ts
@@ -27,12 +27,39 @@ export const FileApiDefinition = {
   },
 
   /**
-   * 上传分块文件
+   * 合并分块文件
    */
   mergeChunk: {
     method: 'post' as const,
     server: 'merge/chunk' as const,
     client: () => '/file/merge/chunk',
+  },
+
+  /**
+   * OSS 签名
+   */
+  ossSign: {
+    method: 'post' as const,
+    server: 'oss/sign' as const,
+    client: () => '/file/oss/sign',
+  },
+
+  /**
+   * OSS 分块上传
+   */
+  ossChunk: {
+    method: 'post' as const,
+    server: 'oss/chunk' as const,
+    client: () => '/file/oss/chunk',
+  },
+
+  /**
+   * OSS 合并分块
+   */
+  ossMerge: {
+    method: 'post' as const,
+    server: 'oss/merge' as const,
+    client: () => '/file/oss/merge',
   },
 };
 
